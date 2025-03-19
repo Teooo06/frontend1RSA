fb09f706202fe98f24e69ffb12a88dccf69ac7f1
Teooo06 <matteobertoldini06@gmail.com>
2025-03-19 08:37:30 +0100

    Aumentata la larghezza per tutto lo schermo e aggiunta file e cambio nome

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,8 @@ const App = () => {
     };
 
     return (
-        <div className="min-h-screen bg-gray-50 w-100%">
+        <div className="min-h-screen bg-gray-50"
+        style={{width: "100%"}}>
             {/* Header */}
             <header className="bg-white shadow-md">
                 <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
